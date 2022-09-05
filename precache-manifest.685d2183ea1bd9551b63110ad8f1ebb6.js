d41bd424ce5160fdaa1f14758cf893e60fb9ab5b
vijay meena <jmeena125@gmail.com>
2022-09-05 21:40:42 +0530

    Updates

---
+++ precache-manifest.685d2183ea1bd9551b63110ad8f1ebb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "31db56225cc5a06aefe5",
+    "url": "/Robo_Friends_App/static/css/main.f9c7adea.chunk.css"
+  },
+  {
+    "revision": "31db56225cc5a06aefe5",
+    "url": "/Robo_Friends_App/static/js/main.3cd9584b.chunk.js"
+  },
+  {
+    "revision": "f3733a15c25714b8d56e",
+    "url": "/Robo_Friends_App/static/js/runtime~main.1a584107.js"
+  },
+  {
+    "revision": "d1ad0101ca430874e9b0",
+    "url": "/Robo_Friends_App/static/css/2.cd87d151.chunk.css"
+  },
+  {
+    "revision": "d1ad0101ca430874e9b0",
+    "url": "/Robo_Friends_App/static/js/2.cf92c1d9.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo_Friends_App/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1f842b31f06378cba25fe7a779a559d7",
+    "url": "/Robo_Friends_App/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Robo_Friends_App/precache-manifest.685d2183ea1bd9551b63110ad8f1ebb6.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Robo_Friends_App/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
